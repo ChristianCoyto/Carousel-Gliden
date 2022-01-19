e76b9ece599e0969a85ebd6fa50e60978e301c05
christianCoyto <christiancoyto@gmail.com>
2022-01-19 20:50:25 -0300

    format document

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
 	new Glider(document.querySelector('.carousel__lista'), {
 		slidesToShow: 1,
 		slidesToScroll: 1,
@@ -9,20 +9,20 @@ window.addEventListener('load', function(){
 		},
 		responsive: [
 			{
-			  // screens greater than >= 775px
-			  breakpoint: 450,
-			  settings: {
-				// Set to `auto` and provide item width to adjust to viewport
-				slidesToShow: 2,
-				slidesToScroll: 2
-			  }
-			},{
-			  // screens greater than >= 1024px
-			  breakpoint: 800,
-			  settings: {
-				slidesToShow: 4,
-				slidesToScroll: 4
-			  }
+				// screens greater than >= 775px
+				breakpoint: 450,
+				settings: {
+					// Set to `auto` and provide item width to adjust to viewport
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			}, {
+				// screens greater than >= 1024px
+				breakpoint: 800,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4
+				}
 			}
 		]
 	});
